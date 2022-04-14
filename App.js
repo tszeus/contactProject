9eb440a39fbd5cca3cac5a299f1bb7f93bc4572a
tszeus <sbfzeusa2@gmail.com>
2022-04-15 06:49:08 +0700

    contact.js

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, ScrollView } from "react-native";
+import React from "react";
+import contacts from "./contact";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Row = (props) => (
+  <View style={styles.info}>
+    <Text>{`${props.name}\t\t\t\t\t`}</Text>
+    <Text>{props.phone}</Text>
+  </View>
+);
+
+export default class App extends React.Component {
+  state = {
+    showContacts: false,
+  };
+
+  toggleContact = () => {
+    this.setState((prevState) => ({ showContacts: !prevState.showContacts }));
+  };
+
+  render() {
+    if (!this.state.showContacts) {
+      return (
+        <View style={styles.container}>
+          <Button title="toogle contacts" onPress={this.toggleContact} />
+          <ScrollView>
+            <View>
+              {contacts.map((contact) => (
+                <Row {...contact} />
+              ))}
+            </View>
+          </ScrollView>
+        </View>
+      );
+    }
+    return (
+      <View style={styles.container}>
+        <Button title="toogle contacts" onPress={this.toggleContact} />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  info: {
+    flexDirection: "row",
   },
 });
